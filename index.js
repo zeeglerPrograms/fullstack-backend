107eed5e7415f6d0650a0c1a2c06bd206ebbf3f8
Daniel Zeegler <155176365+zeeglerPrograms@users.noreply.github.com>
2025-09-19 09:49:32 -0500

    backendworking

--- index.js
+++ index.js
@@ -49,14 +49,9 @@ app.get('/api/notes', (request, response) => {
 })
 
 app.get('/api/notes/:id', (request, response) => {
-  const id = request.params.id
-  const note = notes.find(note => note.id === id)
-  
-  if (note) {
+  Note.findById(request.params.id).then(note => {
     response.json(note)
-  } else {
-    response.status(404).end()
-  }
+  })
 })
 
 const generateId = () => { 
@@ -75,15 +70,14 @@ app.post('/api/notes', (request, response) => {
     })
   }
   
-  const note = {
+  const note = new Note({
     content: body.content,
-    important: body.important || false,
-    id: generateId(),
-  }
+    important: body.important || false
+  })
 
-  notes = notes.concat(note)
-  
-  response.json(note)
+  note.save().then(savedNote => {
+    response.json(savedNote)
+  })
 })
 
 app.delete('/api/notes/:id', (request, response) => {
