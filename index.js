04beb11416a248dbeedac097fe7f1751ba4b5fea
Daniel Zeegler <155176365+zeeglerPrograms@users.noreply.github.com>
2025-09-19 15:08:09 -0500

    validatino added

--- index.js
+++ index.js
@@ -15,27 +15,6 @@ const requestLogger = (request, response, next) => {
 }
 app.use(requestLogger)
 
-
-
-
-let notes = [
-  {
-    id: "1",
-    content: "HTML is easy",
-    important: true
-  },
-  {
-    id: "2",
-    content: "Browser can execute only JavaScript",
-    important: false
-  },
-  {
-    id: "3",
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true
-  }
-]
-
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
 })
@@ -58,13 +37,6 @@ app.get('/api/notes/:id', (request, response, next) => {
     .catch(error => next(error))
 
 })
-
-const generateId = () => { 
-  const maxId = notes.length > 0
-    ? Math.max(...notes.map(n => Number(n.id)))
-    : 0
-  return String(maxId + 1)
-}
 
 app.post('/api/notes', (request, response, next) => {
   const body = request.body
--- mongo.js
+++ mongo.js
@@ -1,42 +1,37 @@
 const mongoose = require('mongoose')
 
-if (process.argv.length <3) {
-    console.log('give password as argument')
-    process.exit(1)
+if (process.argv.length < 3) {
+  console.log('give password as argument')
+  process.exit(1)
 }
 
 const password = process.argv[2]
 
-const url = `mongodb+srv://zeeglermusic:${password}@cluster0.vp4c8zn.mongodb.net/noteApp?retryWrites=true&w=majority&appName=Cluster0`
+const url = `mongodb+srv://fullstack:${password}@cluster0.a5qfl.mongodb.net/noteApp?retryWrites=true&w=majority&appName=Cluster0`
 
 mongoose.set('strictQuery', false)
-
 mongoose.connect(url)
 
 const noteSchema = new mongoose.Schema({
-    content: {
-        type: String,
-        minLength: 5,
-        required: true
-    },
-    important: Boolean,
+  content: String,
+  important: Boolean,
 })
 
 const Note = mongoose.model('Note', noteSchema)
 
 // const note = new Note({
-//     content: 'Adding a new note',
-//     important: false,
+//   content: 'HTML is easy',
+//   important: true,
 // })
 
-// note.save().then(result => {
-//     console.log('note saved!')
-//     mongoose.connection.close()
-// })
+// // note.save().then((result) => {
+// //   console.log('note saved!')
+// //   mongoose.connection.close()
+// // })
 
-// Note.find({important: true}).then(result => {
-//     result.forEach(note => {
-//         console.log(note)
-//     })
-//     mongoose.connection.close()
-// })
+Note.find({}).then((result) => {
+  result.forEach((note) => {
+    console.log(note)
+  })
+  mongoose.connection.close()
+})
