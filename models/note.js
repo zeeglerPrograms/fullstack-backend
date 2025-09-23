94b40038eac06e03493051850b2fe4ee7e5b262f
Daniel Zeegler <155176365+zeeglerPrograms@users.noreply.github.com>
2025-09-23 14:09:38 -0500

    refactored

--- models/note.js
+++ models/note.js
@@ -1,27 +1,13 @@
 const mongoose = require('mongoose')
 
-mongoose.set('strictQuery', false)
-
-const url = process.env.MONGODB_URI
-
-console.log('connecting to ', url)
-mongoose.connect(url)
-  .then(() => {
-    console.log('connected to MongoDB')
-  })
-  .catch(error => {
-    console.log('error connecting to MongoDb:', error.message)
-  })
-
 const noteSchema = new mongoose.Schema({
   content: {
     type: String,
-    minlength: 5,
     required: true,
+    minlength: 5
   },
   important: Boolean,
 })
-
 
 noteSchema.set('toJSON', {
   transform: (document, returnedObject) => {
