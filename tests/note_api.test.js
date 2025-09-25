da316267ac6f7ca8a883753d0d9b6eeb126a25f0
Daniel Zeegler <155176365+zeeglerPrograms@users.noreply.github.com>
2025-09-25 10:32:50 -0500

    end of 4.12

--- tests/note_api.test.js
+++ tests/note_api.test.js
@@ -10,12 +10,7 @@ const api = supertest(app)
 
 beforeEach(async () => {
   await Note.deleteMany({})
-
-  let noteObject = new Note(helper.initialNotes[0])
-  await noteObject.save()
-
-  noteObject = new Note(helper.initialNotes[1])
-  await noteObject.save()
+  await Note.insertMany(helper.initialNotes)
 })
 
 test('notes are returned as json', async () => {
